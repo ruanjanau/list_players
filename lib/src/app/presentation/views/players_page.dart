45132c9f731af47b657a4b28c2a2d2c82efc120c
ruanjanau <rhuanjanau531@gmail.com>
2023-12-08 19:05:00 -0400

    feat create players page

---
+++ lib/src/app/presentation/views/players_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlayersPage extends StatelessWidget {
+  const PlayersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Players'),
+      ),
+      body: Container(),
+    );
+  }
+}
