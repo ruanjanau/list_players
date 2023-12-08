ab2b90356db44533b96aa8480a64e2f8e1372f63
ruanjanau <rhuanjanau531@gmail.com>
2023-12-08 19:04:08 -0400

    feat create entity

---
+++ lib/src/app/domain/entities/players_entity.dart
@@ -0,0 +1,17 @@
+class PlayersEntity {
+  final String id;
+  final String name;
+  final int idade;
+  final String aniversario;
+  final String profession;
+  final String image;
+
+  PlayersEntity({
+    required this.id,
+    required this.name,
+    required this.idade,
+    required this.aniversario,
+    required this.profession,
+    required this.image,
+  });
+}
