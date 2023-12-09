4c5dc495c5fe5bb93ea68758c3277d56eb55387a
ruanjanau <rhuanjanau531@gmail.com>
2023-12-09 16:15:26 -0400

    create blocprovider routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:list_players/src/app/data/datasources/datasource.dart';
+import 'package:list_players/src/app/presentation/views/bloc/players_bloc.dart';
+import 'package:list_players/src/app/presentation/views/players.dart';
 
-import 'src/app/presentation/views/players.dart';
+import 'src/app/presentation/views/home/home.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  Dio dio = Dio();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+  runApp(
+    RepositoryProvider(
+      create: (context) => PlayersDataSource(dio),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/home',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routes: {
+          '/home': (_) => const HomePage(),
+          '/bloc/players': (context) => BlocProvider(
+                create: (_) =>
+                    PlayersBloc(dataSource: context.read<PlayersDataSource>())
+                      ..add(PlayersEvent.findAll()),
+                child: const PlayersPage(),
+              ),
+        },
       ),
-      home: const PlayersPage(),
-    );
-  }
+    ),
+  );
 }
