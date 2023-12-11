ccbd04b241e9ace0ced925d44f9ee3aa0c8c1464
ruanjanau <rhuanjanau531@gmail.com>
2023-12-11 18:56:40 -0400

    alter model

--- lib/src/app/data/models/players_model.dart
+++ lib/src/app/data/models/players_model.dart
@@ -3,7 +3,7 @@ import 'package:list_players/src/app/domain/entities/enitity.dart';
 class PlayersModel {
   final int? id;
   final String? name;
-  final int? idade;
+  final String? idade;
   final String? aniversario;
   final String? profession;
   final String? team;
@@ -25,7 +25,7 @@ class PlayersModel {
     return PlayersModel(
       id: map['id']?.toInt(),
       name: map['name'] ?? '',
-      idade: map['idade']?.toInt(),
+      idade: map['idade'] ?? '',
       aniversario: map['aniversario'] ?? '',
       profession: map['profession'] ?? '',
       team: map['team'] ?? '',
@@ -38,7 +38,7 @@ class PlayersModel {
     return PlayersEntity(
       id: id ?? 0,
       name: name ?? '',
-      idade: idade ?? 0,
+      idade: idade ?? '',
       aniversario: aniversario ?? '',
       profession: profession ?? '',
       team: team ?? '',
