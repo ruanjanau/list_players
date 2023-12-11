dbc9d674dc65544f4a9ea547372fd9be4e3c5803
ruanjanau <rhuanjanau531@gmail.com>
2023-12-11 18:51:51 -0400

    alter main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart'; // Adicione esta importação
-import 'package:dio/dio.dart'; // Adicione esta importação se ainda não estiver importada
-
+import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
 import 'src/app/data/datasources/datasource.dart';
+import 'src/app/data/models/model.dart';
 import 'src/app/presentation/views/bloc/players_bloc.dart';
-import 'src/app/presentation/views/players.dart';
-
 import 'src/app/presentation/views/home/home.dart';
+import 'src/app/presentation/views/pleyers/players.dart';
 
 void main() {
   runApp(
@@ -17,32 +16,28 @@ void main() {
           create: (_) => PlayersDataSource(Dio()),
         ),
       ],
-      child: const MyApp(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/home',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routes: {
+          '/home': (_) => const HomePage(),
+          '/bloc/players/': (context) => BlocProvider(
+                create: (context) =>
+                    PlayersBloc(dataSource: context.read<PlayersDataSource>())
+                      ..add(const PlayersEvent.findAll()),
+                child: const PlayersPage(),
+              ),
+          '/bloc/player_details/': (context) {
+            final player =
+                ModalRoute.of(context)!.settings.arguments as PlayersModel;
+            return PlayerDetails(player: player);
+          },
+        },
+      ),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      routes: {
-        '/home': (_) => const HomePage(),
-        '/bloc/players/': (context) => BlocProvider(
-              create: (context) =>
-                  PlayersBloc(dataSource: context.read<PlayersDataSource>())
-                    ..add(const PlayersEvent.findAll()),
-              child: const PlayersPage(),
-            ),
-      },
-    );
-  }
 }
